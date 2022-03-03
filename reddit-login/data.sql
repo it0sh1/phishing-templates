4cc560c0f95b3a985720e1b6d42ab359be77177e
It0sh1 <52290766+it0sh1@users.noreply.github.com>
2022-03-03 10:27:40 +0100

    Delete reddit-login directory

--- reddit-login/data.sql
+++
@@ -1,8 +0,0 @@
--- You can use this query if you want your victims
--- user input into a database.
-CREATE TABLE stolen_data (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  stolen_username VARCHAR(100) NOT NULL,
-  stolen_password VARCHAR(255) NOT NULL,
-  death_day DATETIME DEFAULT CURRENT_TIMESTAMP
-);
